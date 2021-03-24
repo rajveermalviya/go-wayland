94544da374a5e20b98e59a0b188756af0a8ff4c3
rajveermalviya <rajveer0malviya@gmail.com>
2021-03-24 15:31:24 +0530

    add text-input v1 & v3 protocols

---
+++ unstable/text-input-v3/doc.go
@@ -0,0 +1,3 @@
+package text_input
+
+//go:generate go run ../../cmd/go-wayland-scanner/scanner.go -pkg text_input -i https://gitlab.freedesktop.org/wayland/wayland-protocols/-/raw/d10d18f3d49374d2e3eb96d63511f32795aab5f7/unstable/text-input/text-input-unstable-v3.xml -o text_input.go
