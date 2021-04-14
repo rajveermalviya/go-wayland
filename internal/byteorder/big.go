06a5fac98307218b09f535f548d91867743d7457
rajveermalviya <rajveer0malviya@gmail.com>
2021-04-14 17:22:54 +0530

    update byteorder list from golang.org/x/sys

--- internal/byteorder/big.go
+++ internal/byteorder/big.go
@@ -1,4 +1,6 @@
-// +build armbe arm64be mips mips64 mips64p32 ppc ppc64 sparc sparc64 s390 s390x
+// +build armbe arm64be m68k mips mips64 mips64p32 ppc ppc64 s390 s390x shbe sparc sparc64
+
+// from golang.org/x/sys
 
 package byteorder
 
--- internal/byteorder/little.go
+++ internal/byteorder/little.go
@@ -1,4 +1,6 @@
-// +build 386 amd64 amd64p32 arm arm64 mipsle mis64le mips64p32le ppc64le riscv riscv64 wasm
+// +build 386 amd64 amd64p32 alpha arm arm64 mipsle mips64le mips64p32le nios2 ppc64le riscv riscv64 sh wasm
+
+// from golang.org/x/sys
 
 package byteorder
 
